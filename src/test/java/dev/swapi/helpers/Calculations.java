8306fb1ba06d068f43bc3b9f9b64bfb8784aafcd
aiandmlengineer <aiandmlengineer@gmail.com>
2022-01-29 13:01:43 -0500

    Removed comment.

--- src/test/java/dev/swapi/helpers/Calculations.java
+++ src/test/java/dev/swapi/helpers/Calculations.java
@@ -10,25 +10,4 @@ public class Calculations {
         long size = Arrays.stream(Arrays.stream(heightNum).toArray()).filter(e -> e > 200).count();
         return size;
     }
-
-
-
-
-
-
-
-
-
-
-
-//    public static long countCorrectHeight2(String[] height){
-//        System.out.println("Length " + height.length);
-//        System.out.println(Arrays.toString(height));
-//        int[] heightNum = Stream.of(height).mapToInt(Integer::parseInt).toArray();
-//        System.out.println("----------------------------------------------");
-//        System.out.println("Length " + heightNum.length);
-//        System.out.println(Arrays.toString(heightNum));
-//        long size = Arrays.stream(Arrays.stream(heightNum).toArray()).filter(e -> e > 200).count();
-//        return size;
-//    }
 }
